055f81c6fdbc26a83c1ce8e7949ff40985970133
Aldayr <aldayr.ruiz@opendeusto.es>
2021-03-02 14:14:25 +0100

    Remove user non-authentication automatically generated

--- api/views.py
+++ api/views.py
@@ -5,13 +5,6 @@ from django.shortcuts import get_object_or_404
 from api.permissions import IsVehicleAccessible, get_vehicles
 
 
-# Use this function for development purposes while authentication is not available.
-# When authentication is ready delete this func, the errors where this func were used will show up.
-# Fix the errors getting the user by self.request.user.
-def get_aldayr():
-    return User.objects.get(username='aldayr')
-
-
 def is_vehicle_accessible(user, vehicle_id):
     return get_vehicles(user).filter(id=vehicle_id).exists()
 
@@ -22,7 +15,7 @@ class VehicleViewSet(viewsets.ViewSet):
         """
         Returns a list of vehicles user requester has access.
         """
-        user = get_aldayr()
+        user = self.request.user
         queryset = get_vehicles(user)
         serializer = VehicleSerializer(queryset, many=True)
         return Response(serializer.data)
@@ -31,7 +24,7 @@ class VehicleViewSet(viewsets.ViewSet):
         """
         Returns the vehicle if user requester has access.
         """
-        user = get_aldayr()
+        user = self.request.user
         queryset = get_vehicles(user)
         vehicle = get_object_or_404(queryset, pk=pk)
         serializer = VehicleSerializer(vehicle)
@@ -45,7 +38,7 @@ class VehicleTypeViewSet(viewsets.ViewSet):
         Returns a list of vehicle types that user requester has access.
         It could be useful to filter later.
         """
-        user = get_aldayr()
+        user = self.request.user
         queryset = user.allowed_types.all()
         serializer = VehicleTypeSerializer(queryset, many=True)
         return Response(serializer.data)
@@ -55,7 +48,7 @@ class VehicleTypeViewSet(viewsets.ViewSet):
         Returns a vehicle type if user requester has access.
         It should return vehicles list too or filter them by url in VehicleViewSet.
         """
-        user = get_aldayr()
+        user = self.request.user
         queryset = user.allowed_types.all()
         vehicle_type = get_object_or_404(queryset, pk=pk)
         serializer = VehicleTypeSerializer(vehicle_type)
@@ -65,13 +58,13 @@ class VehicleTypeViewSet(viewsets.ViewSet):
 class ReservationViewSet(viewsets.ViewSet):
 
     def list(self, request):
-        user = get_aldayr()
+        user = self.request.user
         queryset = user.reservations.all()
         serializer = ReservationSerializer(queryset, many=True)
         return Response(serializer.data)
 
     def create(self, request):
-        user = get_aldayr()
+        user = self.request.user
         serializer = CreateReservationSerializer(data=self.request.data)
 
         # Verify if you have access to the vehicle. Try to permission_classes later, I couldn't do it.
@@ -87,16 +80,16 @@ class ReservationViewSet(viewsets.ViewSet):
 
     # UNCOMMENT WHEN AUTHENTICATION IN CLIENT SIDE IS IMPLEMENTED. REMOVE is_vehicle_accessible()
     # THIS RESTRICT TO REQUESTER MAKE A RESERVATION OF VEHICLE FLEET AND VEHICLE TYPES THAT HE DOESN'T HAVE ACCESS.
-    # def get_permissions(self):
-    #     """
-    #     Instantiates and returns the list of permissions that this view requires.
-    #     """
-    #     if self.action == 'list':
-    #         permission_classes = [permissions.IsAuthenticated]
-    #     else:
-    #         # You must be authenticated and have access to the vehicle.
-    #         permission_classes = [permissions.IsAuthenticated, IsVehicleAccessible]
-    #     return [permission() for permission in permission_classes]
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'list':
+            permission_classes = [permissions.IsAuthenticated]
+        else:
+            # You must be authenticated and have access to the vehicle.
+            permission_classes = [permissions.IsAuthenticated, IsVehicleAccessible]
+        return [permission() for permission in permission_classes]
 
 
 class UserList(generics.ListAPIView):
