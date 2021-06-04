ffedc215c4e98948a9e5aebf08073b5daf0463df
aldayr <aldayr.ruiz@opendeusto.es>
2021-06-04 16:55:23 +0200

    feat: Single user serializer for select allowed vehicles

--- applications/users/serializers/simple.py
+++ applications/users/serializers/simple.py
@@ -3,9 +3,7 @@ from rest_framework import serializers
 
 
 class SimpleUserSerializer(serializers.ModelSerializer):
-    # allowed_types = SimpleVehicleTypeSerializer(read_only=True, many=True)
 
     class Meta:
         model = get_user_model()
-        # fields = ['id', 'email', 'username', 'date_joined', 'allowed_types']
         fields = ['id', 'email', 'username', 'date_joined']
