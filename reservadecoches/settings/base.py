ef373faf4f8589549c5b3c5fcef11e382ce73d3a
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2021-06-15 12:30:33 +0200

    refactor: Remove little mistake at base settings.

--- reservadecoches/settings/base.py
+++ reservadecoches/settings/base.py
@@ -1,7 +1,4 @@
 from pathlib import Path
-import mimetypes
-
-mimetypes.add_type("text/javascript", ".js", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
