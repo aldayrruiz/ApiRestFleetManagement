ad35973a7e5874dcdbc38d5b00a0a85cac34a3c6
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2021-06-15 19:25:28 +0200

    feat: Remove login endpoint and fix naming field of a serializer

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import re_path
-from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 
 from applications.allowed_vehicles.views import AllowedVehicleViewSet
@@ -22,6 +21,5 @@ router.register(r'register', RegistrationViewSet, basename='register')
 urlpatterns = router.urls
 
 urlpatterns += [
-    re_path(r'^user/login/?$', obtain_auth_token, name='user_login'),
-    re_path(r'^admin/login/?$', AdminAuthToken.as_view(), name='admin_login')
+    re_path(r'^login/?$', AdminAuthToken.as_view(), name='login')
 ]
