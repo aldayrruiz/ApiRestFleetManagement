a2119a6c8029fbe3a4754bb2551621257af283f0
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2021-07-14 19:19:28 +0200

    fix: Solve Ticket

--- applications/reservations/utils.py
+++ applications/reservations/utils.py
@@ -1,10 +1,14 @@
+import logging
 from datetime import datetime
 
 import pytz
 
-from utils.email.tickets import send_accepted_ticket_email
+from applications.tickets.models import TicketStatus
+from utils.email.tickets import send_accepted_ticket_email, send_denied_ticket_email
 
 TIMEZONE = 'UTC'
+
+logger = logging.getLogger(__name__)
 
 
 def is_reservation_already_started(reservation):
@@ -17,10 +21,30 @@ def is_reservation_already_ended(reservation):
     return reservation.end < now
 
 
-def delete_reservation(reservation):
+def delete_reservation(reservation, new_ticket_st=TicketStatus.DENIED):
+    """
+    Delete reservation and send email accepting or denying ticket depending on new_ticket_st to ticket owners.
+    It does not send email of reservation deleted to owner.
+
+    :param reservation: Reservation to delete
+    :param new_ticket_st: Status to solve all reservation's tickets
+    :return: None or Raise a exception if new_ticket_st not in [ACCEPTED, DENIED]
+    """
+
+    if is_reservation_already_started(reservation):
+        logger.exception('Cannot delete a reservation which has already started: {}'.format(reservation.id))
+
+    if new_ticket_st is TicketStatus.ACCEPTED:
+        send_email = send_accepted_ticket_email
+    elif new_ticket_st is TicketStatus.DENIED:
+        send_email = send_denied_ticket_email
+    else:
+        logger.exception('Cannot delete a reservation and solve their ticket with a status: {}'.format(new_ticket_st))
+        return
+
     tickets = reservation.tickets.all()
     for ticket in tickets:
-        send_accepted_ticket_email(ticket)
+        send_email(ticket)
     reservation.delete()
 
 
--- applications/tickets/services/solver.py
+++ applications/tickets/services/solver.py
@@ -1,20 +1,26 @@
+import logging
+
 from applications.reservations.utils import is_reservation_already_started, delete_reservation
 from applications.tickets.models import TicketStatus
 from utils.email.tickets import send_reservation_deleted_email, send_accepted_ticket_email, send_denied_ticket_email
+
+logger = logging.getLogger(__name__)
 
 
 def solve_ticket(ticket, new_status):
     reservation = ticket.reservation
     if is_reservation_already_started(reservation):
-        return "Can't solve a reservation already started"
+        logger.exception('Cannot delete a reservation which has already started: {}'.format(reservation.id))
 
     if new_status == TicketStatus.UNSOLVED:
-        return "Can't solve a ticket to unsolved status"
+        logger.exception('Cannot solve a ticket with status UNSOLVED')
     elif new_status == TicketStatus.ACCEPTED:
         send_reservation_deleted_email(reservation)
         send_accepted_ticket_email(ticket)
-        delete_reservation(reservation)
+        ticket.delete()
+        delete_reservation(reservation, new_ticket_st=TicketStatus.DENIED)
     elif new_status == TicketStatus.DENIED:
         send_denied_ticket_email(ticket)
+        ticket.delete()
     else:
-        return "Unsupported status"
+        logger.exception('Unsupported')
