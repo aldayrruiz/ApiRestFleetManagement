b7ded3ee9426d1f3c924ff3514510638f88f1198
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2021-07-30 12:53:54 +0200

    feat: Prepare to receive photoBase64 in incident creation

--- applications/incidents/serializers/create.py
+++ applications/incidents/serializers/create.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 
 from applications.incidents.models import Incident
+from drf_extra_fields.fields import Base64ImageField
 
 
 class CreateIncidentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     solved = serializers.BooleanField(read_only=True)
+    photo = Base64ImageField(required=False)
 
     class Meta:
         model = Incident
-        fields = ['id', 'title', 'description', 'owner', 'reservation', 'type', 'solved']
+        fields = ['id', 'title', 'description', 'owner', 'reservation', 'type', 'solved', 'photo']
