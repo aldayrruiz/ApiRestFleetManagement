f23d1eff009845b730e89b379b93f9cc016d1015
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2021-11-18 11:02:08 +0100

    feat: Add vehicles list preference to create_by_date

--- applications/reservations/services/bydate/creator.py
+++ applications/reservations/services/bydate/creator.py
@@ -1,25 +1,26 @@
 from operator import itemgetter
 
-from applications.allowed_vehicles.services.queryset import get_allowed_vehicles_queryset
+from applications.allowed_vehicles.services.queryset import get_allowed_vehicles_queryset, get_vehicles_ordered_by_ids
 from applications.reservations.models import Reservation
 from applications.reservations.services.validator import ReservationValidator
 from utils.dates import from_naive_to_aware
 
 
 class ReservationByDateCreator:
-    def __init__(self, title, description, start, end):
+    def __init__(self, title, description, start, end, vehicles, owner):
         self.title = title
         self.description = description
         self.start = from_naive_to_aware(start)
         self.end = from_naive_to_aware(end)
+        self.vehicles = vehicles
+        self.owner = owner
 
-    def create(self, requester):
+    def create(self):
         """
         Return reservation created. Otherwise, None is returned.
         """
-        vehicles = get_allowed_vehicles_queryset(requester)
-        for vehicle in vehicles:
-            possible_reservation = self.__get_reservation__(requester, vehicle)
+        for vehicle in self.vehicles:
+            possible_reservation = self.__get_reservation__(self.owner, vehicle)
             validator = ReservationValidator(vehicle)
             is_valid = validator.is_reservation_valid(possible_reservation)
             if is_valid:
@@ -40,16 +41,21 @@ class ReservationByDateCreator:
         return reservation
 
     @staticmethod
-    def from_serializer(serializer):
+    def from_serializer(serializer, owner):
         # Just taking all variables from body request
         (
             title,
             description,
             start,
             end,
+            vehicles_ids
         ) = itemgetter('title',
                        'description',
                        'start',
-                       'end')(serializer.validated_data)
+                       'end',
+                       'vehicles')(serializer.validated_data)
 
-        return ReservationByDateCreator(title, description, start, end)
+        # Get vehicles ordered by user preference
+        vehicles = get_vehicles_ordered_by_ids(vehicles_ids, owner)
+
+        return ReservationByDateCreator(title, description, start, end, vehicles, owner)
