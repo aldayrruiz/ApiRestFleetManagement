086d6ee5b171142bd7dc884b912cfd019578fef9
Aldayr Ruiz <45878892+aldayrruiz@users.noreply.github.com>
2021-11-24 21:56:55 +0100

    fix: Strict validators creating reservation

---
+++ applications/reservations/serializers/validator.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from utils.dates import is_after_now
+
+
+def validate(data):
+    if not is_after_now(data['start']):
+        raise serializers.ValidationError('No se puede reservar para el pasado.')
+
+    # Check reservation's start date < end date
+    if data['start'] > data['end']:
+        raise serializers.ValidationError("La fecha de la comienzo debe ser anterior a la final")
+
+    if (data['end'] - data['start']).days > 3:
+        raise serializers.ValidationError('No se puede reservar por mas de 3 días.')
