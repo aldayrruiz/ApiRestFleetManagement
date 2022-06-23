63e53a960ac2cd92f7a556cb29300794fe1e7e65
Aldayr Ruiz <45878892+aldayrruiz@users.noreply.github.com>
2022-06-23 10:59:51 +0200

    chore: Makemigrations and GithubActions .env variable

--- src/applications/reports/models/punctuality.py
+++ src/applications/reports/models/punctuality.py
@@ -5,11 +5,11 @@ from applications.tenants.models import Tenant
 
 
 class PunctualityReport(AbstractReport):
-    take_out = models.DecimalField()  # Recogido antes de que la reserva haya comenzado (horas).
-    take_in = models.DecimalField()  # Recogido después de que la reserva haya comenzado (horas).
-    free_in = models.DecimalField()  # Liberado antes de que la reserva haya terminado (horas).
-    free_out = models.DecimalField()  # Liberado después de que la reserva haya terminado (horas).
-    not_taken = models.DecimalField()  # La reserva no se ha levado acabo (horas).
+    take_out = models.DecimalField(max_digits=10, decimal_places=5)  # Recogido antes de que la reserva haya comenzado (horas).
+    take_in = models.DecimalField(max_digits=10, decimal_places=5)  # Recogido después de que la reserva haya comenzado (horas).
+    free_in = models.DecimalField(max_digits=10, decimal_places=5)  # Liberado antes de que la reserva haya terminado (horas).
+    free_out = models.DecimalField(max_digits=10, decimal_places=5)  # Liberado después de que la reserva haya terminado (horas).
+    not_taken = models.DecimalField(max_digits=10, decimal_places=5)  # La reserva no se ha levado acabo (horas).
     tenant = models.ForeignKey(Tenant, related_name='punctuality_reports', on_delete=models.CASCADE)
 
     class Meta:
